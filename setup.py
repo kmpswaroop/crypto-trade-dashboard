200a52f862a8de51ba0b85a44a0487afbdee773a
kmpswaroop <pranava.swaroop@protonmail.com>
2020-03-06 12:29:46 +0530

    Initialised project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='crypto_trade_db',
+   version='1.0.0',
+   description='Module to provide trade stream data from Binance Exchange',
+   author='Pranava Swaroop',
+   author_email='pranava.swaroop@protonmail.com',
+   packages=['dashboard'],
+)
